7586eaa30e7226f0d8b6fd4470a85cb6cf50e74b
Daria Approvato <dariaapprovato@gmail.com>
2023-01-02 20:57:18 -0500

    commit tree 1fadb292c734e62c58a54dd95686a15006969d5f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
 
+    attr_accessor :total, :title, :price
+
+    def initialize(total = 0)
+        @total = total
+    end
+
+    def add_item(title, price)
+        @title = title
+        @price = price
+    end
+
+    def add(title, ttl)
+        (self.total + ttl)
+    end
+end
